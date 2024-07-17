da27dfe16993b4663153422335778d0bee3aa7d2
umarorakzai012 <122780654+umarorakzai22k4084@users.noreply.github.com>
2024-07-18 00:22:55 +0500

    addToCart implemented in CartRepo

--- lib/repo/cart.dart
+++ lib/repo/cart.dart
@@ -5,10 +5,10 @@ import 'package:ecommerce_app/models/cart.dart';
 import 'package:http/http.dart' as http;
 
 class CartRepo {
-  static const String _url = 'https://fakestoreapi.com/carts/1';
+  static const String _url = 'https://fakestoreapi.com/carts';
 
   static Future<List<CartItemModel>> getAllCartItems() async {
-    var uri = Uri.parse(_url);
+    var uri = Uri.parse('$_url/1');
     final responce = await http.get(uri);
 
     if (responce.statusCode == 200) {
@@ -24,6 +24,17 @@ class CartRepo {
       return cartItems;
     } else {
       throw Exception('failed to fetch cart');
+    }
+  }
+
+  static Future<bool> addProductToCart(Map<String, dynamic> body) async {
+    var uri = Uri.parse(_url);
+    var responce = await http.post(uri, body: body);
+
+    if (responce.statusCode == 200) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
